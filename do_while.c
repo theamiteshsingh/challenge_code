2d4812ea7193d90d50f5a4f29396e2a20f4eabc8
Amitesh Singh <71075043+theamiteshsingh@users.noreply.github.com>
2022-01-20 01:47:52 +0530

    Add files via upload

---
+++ do_while.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int n;
+int main()
+{
+    do
+    {
+        printf("\nEnter any month{in digits}:: ");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 1:
+            printf("its janaury");
+            break;
+        case 2:
+            printf("its feb");
+            break;
+        case 3:
+            printf("its mar");
+            break;
+        case 4:
+            printf("its apr");
+            break;
+        case 5:
+            printf("its may");
+            break;
+        case 6:
+            printf("its june");
+            break;
+        case 7:
+            printf("its july");
+            break;
+        case 8:
+            printf("its aug");
+            break;
+        case 9:
+            printf("its sept");
+            break;
+        case 10:
+            printf("its oct");
+            break;
+        case 11:
+            printf("Its november");
+            break;
+        case 12:
+            printf("Its december");
+            break;
+        default:
+            printf("Wrong input...................Try again!!!!");
+            break;
+        }
+    } while (n != 13);
+    return 0;
+}
---
+++ switch_case.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int n;
+int main()
+{
+    int n;
+    printf("\nEnter any month{in digits}:: ");
+    scanf("%d", &n);
+    switch (n)
+    {
+    case 1:
+        printf("its janaury");
+        break;
+    case 2:
+        printf("its feb");
+        break;
+    case 3:
+        printf("its mar");
+        break;
+    case 4:
+        printf("its apr");
+        break;
+    case 5:
+        printf("its may");
+        break;
+    case 6:
+        printf("its june");
+        break;
+    case 7:
+        printf("its july");
+        break;
+    case 8:
+        printf("its aug");
+        break;
+    case 9:
+        printf("its sept");
+        break;
+    case 10:
+        printf("its oct");
+        break;
+    case 11:
+        printf("Its november");
+    case 12:
+        printf("Its december");
+    default:
+        printf("Wrong input...................Try again!!!!");
+        break;
+    }
+    return 0;
+}
