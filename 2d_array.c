2a91bed8cf00ea1eaac647f93e6924e3caa9a559
Amitesh Singh <71075043+theamiteshsingh@users.noreply.github.com>
2022-01-21 23:36:58 +0530

    Update 2d_array.c

--- 2d_array.c
+++ 2d_array.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int a[3][3],i,j;
+    int a[4][4],i,j;
     printf("Array is::");
-    for ( i = 0; i < 3; i++)
+    for ( i = 0; i < 4; i++)
     {
-        for ( j = 0; j < 3; j++)
+        for ( j = 0; j < 4; j++)
         {
             scanf("%d", &a[i][j]);
         }
         
     }
     printf("printed array is:::::\n");
-    for ( i = 0; i < 3; i++)
+    for ( i = 0; i < 4; i++)
     {
-        for ( j = 0; j < 3; j++)
+        for ( j = 0; j < 4; j++)
         {
             printf("%d\t", a[i][j]);
         }
