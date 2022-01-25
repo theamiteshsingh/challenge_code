bfe0e43c1cd25e3204e7b3b6f55947a8489e2de0
Amitesh singh <amiteshsinghkorba@gmail.com>
2022-01-25 12:32:08 +0530

    inti

--- bank.c
+++ bank.c
@@ -3,7 +3,20 @@ int main()
 {
     int i;
     printf("Enter your choice");
+    scanf("%d",&i);
+    switch (i)
+    {
+    case 1:
+        printf("Its jan");
+        break;
+    case 2:
+        printf("Its feb");
+        break;
+    case 3:
+        printf("Its mar");
+        break;
     
-
-
+    default:
+        break;
+    }
 }
