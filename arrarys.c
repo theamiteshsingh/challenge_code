fb77c931ecd8db185be9fa0b27a0a91feb41869b
Amitesh Singh <71075043+theamiteshsingh@users.noreply.github.com>
2022-02-15 23:48:33 +0530

    Update arrarys.c

--- arrarys.c
+++ arrarys.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a[5],i;
+    int a[10],i;
     printf("Enter 5 numbers::");
-    for ( i = 0; i <= 5; i++)
+    for ( i = 0; i <= 10; i++)
     {
         scanf("%d",&a[i]);
     }
     printf("Numbers are:::");
-    for ( i = 0; i <= 5; i++)
+    for ( i = 0; i <= 10; i++)
     {
         printf("a[%d]=%d\n",i , a[i]);
     }
